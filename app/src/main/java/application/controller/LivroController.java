d0aab6eac3c59463933acb0e0ca8abb8bec8cf88
marcoweb <marcocarvalho.web@gmail.com>
2023-11-06 22:47:42 +0000

    Livro - Update

--- app/src/main/java/application/controller/LivroController.java
+++ app/src/main/java/application/controller/LivroController.java
@@ -1,11 +1,17 @@
 package application.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner.Mode;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import application.model.GeneroRepository;
+import application.model.Livro;
 import application.model.LivroRepository;
 
 @Controller
@@ -26,5 +32,36 @@ public class LivroController {
     public String insert(Model model) {
         model.addAttribute("generos", generoRepo.findAll());
         return "livro/insert";
+    }
+
+    @RequestMapping(value = "/insert", method = RequestMethod.POST)
+    public String insert(
+        @RequestParam("titulo") String titulo,
+        @RequestParam("isbn") String isbn,
+        @RequestParam("genero") int idGenero
+    ) {
+        Livro livro = new Livro();
+        livro.setTitulo(titulo);
+        livro.setIsbn(isbn);
+        livro.setGenero(generoRepo.findById(idGenero).get());
+
+        livroRepo.save(livro);
+
+        return "redirect:/livro/list";
+    }
+
+    @RequestMapping("/update")
+    public String update(
+        @RequestParam("id") int id,
+        Model model
+    ) {
+        Optional<Livro> livro = livroRepo.findById(id);
+
+        if(livro.isPresent()) {
+            model.addAttribute("livro", livro.get());
+            return "livro/update";
+        }
+
+        return "redirect:livro/list";
     }
 }
